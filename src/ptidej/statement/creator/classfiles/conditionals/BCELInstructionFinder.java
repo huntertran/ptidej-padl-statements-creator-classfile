a13e417c075b3cad13cba0b122296128b3e5ee8d
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-04-23 22:33:30 -0400

    add missing libraries and fix some unimplemented code

--- src/ptidej/statement/creator/classfiles/conditionals/BCELInstructionFinder.java
+++ src/ptidej/statement/creator/classfiles/conditionals/BCELInstructionFinder.java
@@ -19,6 +19,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.bcel.classfile.AnnotationDefault;
+import org.apache.bcel.classfile.AnnotationEntry;
+import org.apache.bcel.classfile.Annotations;
+import org.apache.bcel.classfile.BootstrapMethods;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.CodeException;
 import org.apache.bcel.classfile.ConstantClass;
@@ -27,14 +32,20 @@ import org.apache.bcel.classfile.ConstantFieldref;
 import org.apache.bcel.classfile.ConstantFloat;
 import org.apache.bcel.classfile.ConstantInteger;
 import org.apache.bcel.classfile.ConstantInterfaceMethodref;
+import org.apache.bcel.classfile.ConstantInvokeDynamic;
 import org.apache.bcel.classfile.ConstantLong;
+import org.apache.bcel.classfile.ConstantMethodHandle;
+import org.apache.bcel.classfile.ConstantMethodType;
 import org.apache.bcel.classfile.ConstantMethodref;
+import org.apache.bcel.classfile.ConstantModule;
 import org.apache.bcel.classfile.ConstantNameAndType;
+import org.apache.bcel.classfile.ConstantPackage;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.ConstantString;
 import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.classfile.ConstantValue;
 import org.apache.bcel.classfile.Deprecated;
+import org.apache.bcel.classfile.EnclosingMethod;
 import org.apache.bcel.classfile.ExceptionTable;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.InnerClass;
@@ -44,7 +55,11 @@ import org.apache.bcel.classfile.LineNumber;
 import org.apache.bcel.classfile.LineNumberTable;
 import org.apache.bcel.classfile.LocalVariable;
 import org.apache.bcel.classfile.LocalVariableTable;
+import org.apache.bcel.classfile.LocalVariableTypeTable;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.MethodParameters;
+import org.apache.bcel.classfile.ParameterAnnotationEntry;
+import org.apache.bcel.classfile.ParameterAnnotations;
 import org.apache.bcel.classfile.Signature;
 import org.apache.bcel.classfile.SourceFile;
 import org.apache.bcel.classfile.StackMap;
@@ -59,11 +74,10 @@ import util.io.ProxyConsole;
 /**
  * @author Stéphane Vaucher
  * @author Yann-Gaël Guéhéneuc
- * @since  2006/03/09
+ * @since 2006/03/09
  */
 public class BCELInstructionFinder implements Visitor {
-	private static final Pattern INSTRUCTION_PATTERN = Pattern
-		.compile("^\\d+: *");
+	private static final Pattern INSTRUCTION_PATTERN = Pattern.compile("^\\d+: *");
 	private BCEL2PADLAdaptor adaptor;
 	private JavaClass currentClass;
 	private final Map instructionMap = new HashMap();
@@ -74,38 +88,32 @@ public class BCELInstructionFinder implements Visitor {
 	public BCEL2PADLAdaptor getAdaptor() {
 		return this.adaptor;
 	}
-	public IConstituentOfOperation[] getConditionalInstructions(
-		final String aClassId,
-		final String aMethodId) {
+
+	public IConstituentOfOperation[] getConditionalInstructions(final String aClassId, final String aMethodId) {
 
 		final Map methodMap = this.methodMap(aClassId);
 		final List conditionalInstructions = (List) methodMap.get(aMethodId);
 		if (conditionalInstructions == null) {
-			ProxyConsole
-				.getInstance()
-				.debugOutput()
-				.print(this.getClass().getName());
-			ProxyConsole
-				.getInstance()
-				.debugOutput()
-				.print(" cannot find instuctions in method \"");
+			ProxyConsole.getInstance().debugOutput().print(this.getClass().getName());
+			ProxyConsole.getInstance().debugOutput().print(" cannot find instuctions in method \"");
 			ProxyConsole.getInstance().debugOutput().print(aClassId);
 			ProxyConsole.getInstance().debugOutput().print('.');
 			ProxyConsole.getInstance().debugOutput().print(aMethodId);
 			ProxyConsole.getInstance().debugOutput().println('"');
 			return new IConstituentOfOperation[0];
-		}
-		else {
-			final IConstituentOfOperation[] arrayOfConditionalInstructions =
-				new IConstituentOfOperation[conditionalInstructions.size()];
+		} else {
+			final IConstituentOfOperation[] arrayOfConditionalInstructions = new IConstituentOfOperation[conditionalInstructions
+					.size()];
 			conditionalInstructions.toArray(arrayOfConditionalInstructions);
 			return arrayOfConditionalInstructions;
 		}
 	}
+
 	private boolean isInstruction(final String aLine) {
 		final Matcher matcher = INSTRUCTION_PATTERN.matcher(aLine);
 		return matcher.find();
 	}
+
 	private Map methodMap(final String aClassId) {
 		Map methodMap = (Map) this.instructionMap.get(aClassId);
 
@@ -116,58 +124,78 @@ public class BCELInstructionFinder implements Visitor {
 
 		return methodMap;
 	}
+
 	/**
 	 * @param adaptor The adaptor to set.
 	 */
 	public void setAdaptor(final BCEL2PADLAdaptor anAdaptor) {
 		this.adaptor = anAdaptor;
 	}
+
 	public String toString() {
 		return this.instructionMap.toString();
 	}
+
 	public void visitCode(final Code aCode) {
 	}
+
 	public void visitCodeException(final CodeException aCodeException) {
 	}
+
 	public void visitConstantClass(final ConstantClass aConstantClass) {
 	}
+
 	public void visitConstantDouble(final ConstantDouble aConstantDouble) {
 	}
+
 	public void visitConstantFieldref(final ConstantFieldref aConstantFieldref) {
 	}
+
 	public void visitConstantFloat(final ConstantFloat aConstantFloat) {
 	}
+
 	public void visitConstantInteger(final ConstantInteger aConstantInteger) {
 	}
-	public void visitConstantInterfaceMethodref(
-		final ConstantInterfaceMethodref aConstantInterfaceMethodref) {
+
+	public void visitConstantInterfaceMethodref(final ConstantInterfaceMethodref aConstantInterfaceMethodref) {
 	}
+
 	public void visitConstantLong(final ConstantLong aConstantLong) {
 	}
-	public void visitConstantMethodref(
-		final ConstantMethodref aConstantMethodref) {
+
+	public void visitConstantMethodref(final ConstantMethodref aConstantMethodref) {
 	}
-	public void visitConstantNameAndType(
-		final ConstantNameAndType aConstantNameAndType) {
+
+	public void visitConstantNameAndType(final ConstantNameAndType aConstantNameAndType) {
 	}
+
 	public void visitConstantPool(final ConstantPool aConstantPool) {
 	}
+
 	public void visitConstantString(final ConstantString aConstantString) {
 	}
+
 	public void visitConstantUtf8(final ConstantUtf8 aConstantUtf8) {
 	}
+
 	public void visitConstantValue(final ConstantValue aConstantValue) {
 	}
+
 	public void visitDeprecated(final Deprecated aDeprecated) {
 	}
+
 	public void visitExceptionTable(final ExceptionTable aExceptionTable) {
 	}
+
 	public void visitField(final Field aField) {
 	}
+
 	public void visitInnerClass(final InnerClass anInnerClass) {
 	}
+
 	public void visitInnerClasses(final InnerClasses someInnerClasses) {
 	}
+
 	public void visitJavaClass(final JavaClass aClass) {
 		this.currentClass = aClass;
 		final Method[] methods = aClass.getMethods();
@@ -176,15 +204,19 @@ public class BCELInstructionFinder implements Visitor {
 			this.visitMethod(methods[i]);
 		}
 	}
+
 	public void visitLineNumber(final LineNumber aLineNumber) {
 	}
+
 	public void visitLineNumberTable(final LineNumberTable aLineNumberTable) {
 	}
+
 	public void visitLocalVariable(final LocalVariable aLocalVariable) {
 	}
-	public void visitLocalVariableTable(
-		final LocalVariableTable aLocalVariableTable) {
+
+	public void visitLocalVariableTable(final LocalVariableTable aLocalVariableTable) {
 	}
+
 	public void visitMethod(final Method aMethod) {
 		// Yann 2008/10/01: The Trick!
 		// This is were the instances of ConditionalInstruction are being
@@ -197,49 +229,129 @@ public class BCELInstructionFinder implements Visitor {
 			final Code code = aMethod.getCode();
 			if (code != null) {
 				final String codeText = code.toString();
-				final BufferedReader reader =
-					new BufferedReader(new StringReader(codeText));
+				final BufferedReader reader = new BufferedReader(new StringReader(codeText));
 
 				String line = reader.readLine();
 				do {
 					if (this.isInstruction(line)) {
 						if (line.indexOf("if") > -1) {
-							conditionalInstructions
-								.add(((StatementFactory) StatementFactory
-									.getInstance()).createIfInstruction(line
-									.toCharArray()));
-						}
-						else if (line.indexOf("switch") > -1) {
+							conditionalInstructions.add(((StatementFactory) StatementFactory.getInstance())
+									.createIfInstruction(line.toCharArray()));
+						} else if (line.indexOf("switch") > -1) {
 							final int parenthesisIndex = line.indexOf('(');
-							final int range =
-								line.substring(parenthesisIndex).split(",").length;
-							conditionalInstructions
-								.add(((StatementFactory) StatementFactory
-									.getInstance()).createSwitchInstruction(
-									line.toCharArray(),
-									range));
+							final int range = line.substring(parenthesisIndex).split(",").length;
+							conditionalInstructions.add(((StatementFactory) StatementFactory.getInstance())
+									.createSwitchInstruction(line.toCharArray(), range));
 						}
 					}
 					line = reader.readLine();
-				}
-				while (line != null);
+				} while (line != null);
 			}
-		}
-		catch (final IOException ioe) {
+		} catch (final IOException ioe) {
 			ioe.printStackTrace(ProxyConsole.getInstance().errorOutput());
 		}
 		methodMap.put(mkey, conditionalInstructions);
 	}
+
 	public void visitSignature(final Signature arg0) {
 	}
+
 	public void visitSourceFile(final SourceFile aSourceFile) {
 	}
+
 	public void visitStackMap(final StackMap aStackMap) {
 	}
+
 	public void visitStackMapEntry(final StackMapEntry aStackMapEntry) {
 	}
+
 	public void visitSynthetic(final Synthetic aSynthetic) {
 	}
+
 	public void visitUnknown(final Unknown aUnknown) {
+	}
+
+	@Override
+	public void visitConstantInvokeDynamic(ConstantInvokeDynamic obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotation(Annotations obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitParameterAnnotation(ParameterAnnotations obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotationEntry(AnnotationEntry obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotationDefault(AnnotationDefault obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitLocalVariableTypeTable(LocalVariableTypeTable obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitEnclosingMethod(EnclosingMethod obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitBootstrapMethods(BootstrapMethods obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitMethodParameters(MethodParameters obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantMethodType(ConstantMethodType obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantMethodHandle(ConstantMethodHandle obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitParameterAnnotationEntry(ParameterAnnotationEntry obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantPackage(ConstantPackage constantPackage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantModule(ConstantModule constantModule) {
+		// TODO Auto-generated method stub
+
 	}
 }
--- src/ptidej/statement/creator/classfiles/loc/BCELLOCFinder.java
+++ src/ptidej/statement/creator/classfiles/loc/BCELLOCFinder.java
@@ -18,6 +18,10 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.bcel.classfile.AnnotationDefault;
+import org.apache.bcel.classfile.AnnotationEntry;
+import org.apache.bcel.classfile.Annotations;
+import org.apache.bcel.classfile.BootstrapMethods;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.CodeException;
 import org.apache.bcel.classfile.ConstantClass;
@@ -26,14 +30,20 @@ import org.apache.bcel.classfile.ConstantFieldref;
 import org.apache.bcel.classfile.ConstantFloat;
 import org.apache.bcel.classfile.ConstantInteger;
 import org.apache.bcel.classfile.ConstantInterfaceMethodref;
+import org.apache.bcel.classfile.ConstantInvokeDynamic;
 import org.apache.bcel.classfile.ConstantLong;
+import org.apache.bcel.classfile.ConstantMethodHandle;
+import org.apache.bcel.classfile.ConstantMethodType;
 import org.apache.bcel.classfile.ConstantMethodref;
+import org.apache.bcel.classfile.ConstantModule;
 import org.apache.bcel.classfile.ConstantNameAndType;
+import org.apache.bcel.classfile.ConstantPackage;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.ConstantString;
 import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.classfile.ConstantValue;
 import org.apache.bcel.classfile.Deprecated;
+import org.apache.bcel.classfile.EnclosingMethod;
 import org.apache.bcel.classfile.ExceptionTable;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.InnerClass;
@@ -43,7 +53,11 @@ import org.apache.bcel.classfile.LineNumber;
 import org.apache.bcel.classfile.LineNumberTable;
 import org.apache.bcel.classfile.LocalVariable;
 import org.apache.bcel.classfile.LocalVariableTable;
+import org.apache.bcel.classfile.LocalVariableTypeTable;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.MethodParameters;
+import org.apache.bcel.classfile.ParameterAnnotationEntry;
+import org.apache.bcel.classfile.ParameterAnnotations;
 import org.apache.bcel.classfile.Signature;
 import org.apache.bcel.classfile.SourceFile;
 import org.apache.bcel.classfile.StackMap;
@@ -57,11 +71,10 @@ import util.io.ProxyConsole;
 /**
  * @author Stéphane Vaucher
  * @author Yann-Gaël Guéhéneuc
- * @since  2006/03/09
+ * @since 2006/03/09
  */
 public class BCELLOCFinder implements Visitor {
-	private static final Pattern INSTRUCTION_PATTERN =
-		Pattern.compile("^\\d+: *");
+	private static final Pattern INSTRUCTION_PATTERN = Pattern.compile("^\\d+: *");
 	private BCEL2PADLAdaptor adaptor;
 	private JavaClass currentClass;
 	private final Map instructionMap = new HashMap();
@@ -72,10 +85,10 @@ public class BCELLOCFinder implements Visitor {
 	public BCEL2PADLAdaptor getAdaptor() {
 		return this.adaptor;
 	}
+
 	private Integer getLOC(final Code aCode) throws IOException {
 		final String codeText = aCode.toString();
-		final BufferedReader reader =
-			new BufferedReader(new StringReader(codeText));
+		final BufferedReader reader = new BufferedReader(new StringReader(codeText));
 
 		String line = reader.readLine();
 		int count = 0;
@@ -84,23 +97,23 @@ public class BCELLOCFinder implements Visitor {
 				count++;
 			}
 			line = reader.readLine();
-		}
-		while (line != null);
+		} while (line != null);
 
 		return new Integer(count);
 	}
-	public Integer getInstructionCount(
-		final String aClassId,
-		final String aMethodId) {
+
+	public Integer getInstructionCount(final String aClassId, final String aMethodId) {
 
 		final Map methodMap = this.methodMap(aClassId);
 		final Integer instCount = (Integer) methodMap.get(aMethodId);
 		return instCount;
 	}
+
 	private boolean isInstruction(final String aLine) {
 		final Matcher matcher = INSTRUCTION_PATTERN.matcher(aLine);
 		return matcher.find();
 	}
+
 	private Map methodMap(final String aClassId) {
 		Map methodMap = (Map) this.instructionMap.get(aClassId);
 
@@ -111,56 +124,78 @@ public class BCELLOCFinder implements Visitor {
 
 		return methodMap;
 	}
+
 	/**
 	 * @param adaptor The adaptor to set.
 	 */
 	public void setAdaptor(final BCEL2PADLAdaptor anAdaptor) {
 		this.adaptor = anAdaptor;
 	}
+
 	public String toString() {
 		return this.instructionMap.toString();
 	}
+
 	public void visitCode(final Code aCode) {
 	}
+
 	public void visitCodeException(final CodeException aCodeException) {
 	}
+
 	public void visitConstantClass(final ConstantClass aConstantClass) {
 	}
+
 	public void visitConstantDouble(final ConstantDouble aConstantDouble) {
 	}
+
 	public void visitConstantFieldref(final ConstantFieldref aConstantFieldref) {
 	}
+
 	public void visitConstantFloat(final ConstantFloat aConstantFloat) {
 	}
+
 	public void visitConstantInteger(final ConstantInteger aConstantInteger) {
 	}
+
 	public void visitConstantInterfaceMethodref(final ConstantInterfaceMethodref aConstantInterfaceMethodref) {
 	}
+
 	public void visitConstantLong(final ConstantLong aConstantLong) {
 	}
+
 	public void visitConstantMethodref(final ConstantMethodref aConstantMethodref) {
 	}
-	public void visitConstantNameAndType(
-		final ConstantNameAndType aConstantNameAndType) {
+
+	public void visitConstantNameAndType(final ConstantNameAndType aConstantNameAndType) {
 	}
+
 	public void visitConstantPool(final ConstantPool aConstantPool) {
 	}
+
 	public void visitConstantString(final ConstantString aConstantString) {
 	}
+
 	public void visitConstantUtf8(final ConstantUtf8 aConstantUtf8) {
 	}
+
 	public void visitConstantValue(final ConstantValue aConstantValue) {
 	}
+
 	public void visitDeprecated(final Deprecated aDeprecated) {
 	}
+
 	public void visitExceptionTable(final ExceptionTable aExceptionTable) {
 	}
+
 	public void visitField(final Field aField) {
 	}
+
 	public void visitInnerClass(final InnerClass anInnerClass) {
 	}
+
 	public void visitInnerClasses(final InnerClasses someInnerClasses) {
 	}
+
 	public void visitJavaClass(final JavaClass aClass) {
 		this.currentClass = aClass;
 		final Method[] methods = aClass.getMethods();
@@ -169,14 +204,19 @@ public class BCELLOCFinder implements Visitor {
 			this.visitMethod(methods[i]);
 		}
 	}
+
 	public void visitLineNumber(final LineNumber aLineNumber) {
 	}
+
 	public void visitLineNumberTable(final LineNumberTable aLineNumberTable) {
 	}
+
 	public void visitLocalVariable(final LocalVariable aLocalVariable) {
 	}
+
 	public void visitLocalVariableTable(final LocalVariableTable aLocalVariableTable) {
 	}
+
 	public void visitMethod(final Method aMethod) {
 		Integer count = null;
 		final String ckey = this.adaptor.adapt(this.currentClass);
@@ -187,22 +227,111 @@ public class BCELLOCFinder implements Visitor {
 			if (code != null) {
 				count = this.getLOC(code);
 			}
-		}
-		catch (final IOException ioe) {
+		} catch (final IOException ioe) {
 			ioe.printStackTrace(ProxyConsole.getInstance().errorOutput());
 		}
 		methodMap.put(mkey, count);
 	}
+
 	public void visitSignature(final Signature arg0) {
 	}
+
 	public void visitSourceFile(final SourceFile aSourceFile) {
 	}
+
 	public void visitStackMap(final StackMap aStackMap) {
 	}
+
 	public void visitStackMapEntry(final StackMapEntry aStackMapEntry) {
 	}
+
 	public void visitSynthetic(final Synthetic aSynthetic) {
 	}
+
 	public void visitUnknown(final Unknown aUnknown) {
+	}
+
+	@Override
+	public void visitConstantInvokeDynamic(ConstantInvokeDynamic obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotation(Annotations obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitParameterAnnotation(ParameterAnnotations obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotationEntry(AnnotationEntry obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAnnotationDefault(AnnotationDefault obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitLocalVariableTypeTable(LocalVariableTypeTable obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitEnclosingMethod(EnclosingMethod obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitBootstrapMethods(BootstrapMethods obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitMethodParameters(MethodParameters obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantMethodType(ConstantMethodType obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantMethodHandle(ConstantMethodHandle obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitParameterAnnotationEntry(ParameterAnnotationEntry obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantPackage(ConstantPackage constantPackage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitConstantModule(ConstantModule constantModule) {
+		// TODO Auto-generated method stub
+
 	}
 }
